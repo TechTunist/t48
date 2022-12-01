9888daf6353fdc33ec6ccc544b9a451c61f79553
Matthew Jackson <mrmpjackson@gmail.com>
2022-12-01 23:15:09 +0000

    initial commit. database file and example sqlite added

---
+++ bookbox.py
@@ -0,0 +1,28 @@
+# present menu to user
+
+selection = ''
+
+# run the menu loop until '0' selected
+while selection.lower() != '0':
+
+    # print the menu and obtain user selection
+    selection = input("""
+    1 - Enter Book
+    2 - Update Book
+    3 - Delete Book
+    4 - Search Book
+    0 - Exit\n""")
+
+    if selection == '1':
+        pass
+    elif selection == '2':
+        pass
+    elif selection == '3':
+        pass
+    elif selection == '4':
+        pass
+    elif selection == '0':
+        print("\n\nExiting program\n")
+        break
+
+
